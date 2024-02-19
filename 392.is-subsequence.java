cc2ad632e525a0fca9229e5256e004721be83d01
Junior Brito <junior.br.pr@gmail.com>
2024-02-20 00:01:51 +0100

    Ugly code == Speed!

--- 392.is-subsequence.java
+++ 392.is-subsequence.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         if (s.isEmpty()) return true;
-        int index = 0;
-        for (int i = 0; i < t.length(); i++) {
-            if (s.charAt(index) == t.charAt(i)){
-                index++;
-                if (index == s.length()) return true;
+        int sIndex = 0;
+        int tIndex = 0;
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        int sLength = s.length();
+        int tLength = t.length();
+        while (tIndex < tLength){
+            if (sArr[sIndex] == tArr[tIndex]){
+                sIndex++;
+                if (sIndex == sLength) return true;
             }
+            tIndex++;
         }
         return false;
     }
