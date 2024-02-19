295997283337139d6728bd84f58db165810089f8
Junior Brito <junior.br.pr@gmail.com>
2024-02-19 22:25:19 +0100

    Better

--- 345.reverse-vowels-of-a-string.java
+++ 345.reverse-vowels-of-a-string.java
@@ -1,33 +1,26 @@
-/*
- * @lc app=leetcode id=345 lang=java
- *
- * [345] Reverse Vowels of a String
- */
-
-import java.util.regex.Pattern;
-
-// @lc code=start
+import java.util.HashSet;
+import java.util.List;
 class Solution {
     public String reverseVowels(String s) {
-        int begin = 0;
-        int end = s.length() - 1;
-        Pattern pattern = Pattern.compile("[aeiou]", Pattern.CASE_INSENSITIVE);
+        HashSet<Character> characters = new HashSet<>(10){};
+        characters.addAll(List.of('a','e','i','o','u','A','E','I','O','U'));
         char[] string = s.toCharArray();
 
+        int begin = 0;
+        int end = s.length() - 1;
         for (int i = 0; i < s.length() - 1; i++) {
             if (end <= begin) return new String(string);
-            while(begin < s.length() - 1 && !pattern.matcher(s.subSequence(begin, begin + 1)).find()){
-                if (begin > end) return new String(string);
+
+            while(begin <= s.length() - 1 && !characters.contains(s.charAt(begin))){
+                if (begin >= end) return new String(string);
                 begin++;
             }
 
-            while (end > 0 && !pattern.matcher(s.subSequence(end, end + 1)).find()){
-                if (end < begin) return new String(string);
+            while (end > 0 && !characters.contains(s.charAt(end))){
+                if (end <= begin) return new String(string);
                 end--;
             }
 
-            if (!pattern.matcher(s.subSequence(begin, begin + 1)).find()
-                    || !pattern.matcher(s.subSequence(end, end + 1)).find()) return new String(string);
             char temp = s.charAt(begin);
             string[begin] = s.charAt(end);
             string[end] = temp;
@@ -37,5 +30,3 @@ class Solution {
         return new String(string);
     }
 }
-// @lc code=end
-
