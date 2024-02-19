419f04ea21e84c2d11dcadf648aafea03706f83e
Junior Brito <junior.br.pr@gmail.com>
2024-02-19 18:56:17 +0100

    Finally top 100% runtime!

--- 605.can-place-flowers.java
+++ 605.can-place-flowers.java
@@ -7,19 +7,19 @@
 // @lc code=start
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int mL = flowerbed.length - 2;
-        for (int i = 0; i < (flowerbed.length > 2 ? flowerbed.length : 1); i++) {
-            if ((i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) &&
-                    flowerbed[i] == 0) {
-                n--;
-                i++;
+        if (n == 0) return true;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (i < flowerbed.length - 1 && flowerbed[i + 1] == 1){
+                i+=2;
+                continue;
             }
-
-            if (i < mL && flowerbed[i + 1] == 1) {
+            if ((i == 0 || flowerbed[i - 1] == 0) && flowerbed[i] == 0) {
+                n--;
+                if (n == 0) return true;
                 i++;
             }
         }
-        return n<=0;
+        return false;
     }
 }
 // @lc code=end
