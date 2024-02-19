5f8f4e6377f34d67e39e793b7198c827aae5da34
Junior Brito <junior.br.pr@gmail.com>
2024-02-19 18:28:51 +0100

    My best

--- 605.can-place-flowers.java
+++ 605.can-place-flowers.java
@@ -7,26 +7,19 @@
 // @lc code=start
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if (n == 0) {
-            return true;
-        }
-
-        int flowers = 0;
-        for (int i = 0; i < flowerbed.length; ) {
-            if (flowerbed[i] == 0 && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) && (i == 0 || flowerbed[i - 1] == 0)) {
-                flowers++;
-                if (i >= flowerbed.length - 2 || flowers >= n) {
-                    break;
-                }
+        int mL = flowerbed.length - 2;
+        for (int i = 0; i < (flowerbed.length > 2 ? flowerbed.length : 1); i++) {
+            if ((i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) &&
+                    flowerbed[i] == 0) {
+                n--;
                 i++;
-            } else {
-                if (i < flowerbed.length - 2 && flowerbed[i + 1] == 1) {
-                    i++;
-                }
             }
-            i++;
+
+            if (i < mL && flowerbed[i + 1] == 1) {
+                i++;
+            }
         }
-        return flowers >= n;
+        return n<=0;
     }
 }
 // @lc code=end
