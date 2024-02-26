89ed44a3e41fdc44e4912e20fd91c931eece158d
Junior Brito <junior.br.pr@gmail.com>
2024-02-26 19:41:00 +0100

    Unnecessary line

--- 206.reverse-linked-list.java
+++ 206.reverse-linked-list.java
@@ -3,10 +3,9 @@ import java.util.Stack;
 class Solution {
     public ListNode reverseList(ListNode head) {
         if(head == null) return null;
-        ListNode prev;
         ListNode current = head;
         while (head.next != null) {
-            prev = head.next;
+            ListNode prev = head.next;
             head.next = prev.next;
             prev.next = current;
             current = prev;
