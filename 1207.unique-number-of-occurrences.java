bdafb16cac6ab61be05db0ccb160bc9a5778616d
Junior Brito <junior.br.pr@gmail.com>
2024-02-26 14:06:15 +0100

    Ez

---
+++ 1207.unique-number-of-occurrences.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> numsSeen = new HashMap<>();
+        boolean unique = false;
+        for (int i = 0; i < arr.length; i++) {
+            numsSeen.put(arr[i], numsSeen.getOrDefault(arr[i], 0) + 1);
+        }
+        HashSet<Integer> tracker = new HashSet<>();
+        for (Integer num : numsSeen.values()) {
+            if (tracker.contains(num)) {
+                return false;
+            } else {
+                tracker.add(num);
+            }
+        }
+        return true;
+    }
+}
